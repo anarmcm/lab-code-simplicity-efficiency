f46262339cd0b45c5a8cfb2e3c70ccda087bbbb0
Ana Recio <ana0096@hotmail.com>
2019-11-17 22:15:42 +0100

    simplifing

--- your-code/challenge-1.py
+++ your-code/challenge-1.py
@@ -11,11 +11,29 @@ code simplicity and efficiency.
 
 print('Welcome to this calculator!')
 print('It can add and subtract whole numbers from zero to five')
-a = input('Please choose your first number (zero to five): ')
-b = input('What do you want to do? plus or minus: ')
-c = input('Please choose your second number (zero to five): ')
 
-if a == 'zero' and b == 'plus'  and c == 'zero':
+
+def num_checker(text_to_ask):
+    while True: 
+        integer = input(text_to_ask)
+
+        if re.match("^[0-5]$", integer) is not None:
+            return int(integer)
+
+        print("Has to be digits between 0 and 5.")
+
+def operation_checker(text_to_ask):
+    while True: 
+
+        print("Has to be + or -.")
+
+a = num_checker('Please choose your first number (zero to five): ')
+b = input('What do you want to do? plus or minus: ')
+c = num_checker('Please choose your second number (zero to five): ')
+
+
+
+""if a == 'zero' and b == 'plus'  and c == 'zero':
     print("zero plus zero equals zero")
 if a == 'zero' and b == 'plus'  and c == 'one':
     print("zero plus one equals one")
@@ -86,7 +104,7 @@ if a == 'five' and b == 'plus'  and c == 'three':
 if a == 'five' and b == 'plus'  and c == 'four':
     print("five plus four equals nine")
 if a == 'five' and b == 'plus'  and c == 'five':
-    print("five plus five equals ten")
+    print("five plus five equals ten")"""
 
 
 if a == 'zero' and b == 'minus' and c == 'zero':
@@ -164,6 +182,6 @@ if a == 'five' and b == 'minus' and c == 'five':
 
 
 if (not a == 'zero' and not a == 'one' and not a == 'two' and not a == 'three' and not a == 'four' and not a == 'five') or (not c == 'zero' and not c == 'one' and not c == 'two' and not c == 'three' and not c == 'four' and not c == 'five') or (not b == 'plus' and not b == 'minus'):
-    print("I am not able to answer this question. Check your input.")
+    print("I am not able to answer this question. Check your input.")""
 
 print("Thanks for using this calculator, goodbye :)")
